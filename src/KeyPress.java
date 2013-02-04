00f1db30b9b1d4cc24660da07afb6a22decf55a7
garrickbrazil <garrickbrazil@gmail.com>
2013-02-04 12:18:45 -0500

    Update src/KeyPress.java
    comments

--- src/KeyPress.java
+++ src/KeyPress.java
@@ -1,17 +1,28 @@
-
-
-
+/********************************************************************
+ * Class: KeyPress
+ * Purpose: holds keypress information
+/*******************************************************************/
 public class KeyPress {
+	
+	// Properties
 	private String press;
 	private long time;
 	
+	/********************************************************************
+ 	 * Constructor: Keypress
+	 * Purpose: constructs default keypress object
+	/*******************************************************************/
 	public KeyPress(String press, long time){
 		this.press = press;
 		this.time = time;
 		
 	}
-	public String getPress(){return this.press;}
 	
+	/********************************************************************
+	 * Accessors
+	 * Purpose: gets corresponding information for keypress
+	/*******************************************************************/
+	public String getPress(){return this.press;}
 	public long getTime(){return this.time;}
 	
 }
